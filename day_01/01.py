45aff7d09aa6d61f6d9e46c6d5275d469a31931f
NerdyOldGeek <nerdyoldgeek@proton.me>
2023-12-01 20:10:21 -0600

    removed print statements

--- day_01/01.py
+++ day_01/01.py
@@ -13,14 +13,9 @@ def get_digits(line):
 with open('input') as file:
     file.seek(0)
     list_of_lines = file.readlines()
-    print(len(list_of_lines))
-    input()
     for line in list_of_lines:
-        print(line.strip())
         data = get_digits(line)
-        print(data)
         digits = int(data[0] + data[-1])
-        print(digits)
         cal_vals.append(digits)
 
     print(sum(cal_vals)) 
